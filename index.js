0af074e61a92345422c2f03050e12fc7c73c04ba
vtrsg <vittorioschmittg@gmail.com>
2023-08-04 04:25:36 -0300

    feat: filtrando os objetos por data e quantidade de horas em cada array

--- index.js
+++ index.js
@@ -14,5 +14,27 @@ const orderedJobs = sortJobs(jobs)
 const startDate = new Date(orderedJobs[0]['Data Máxima de conclusão'])
 const EndDate = new Date(orderedJobs[orderedJobs.length - 1]['Data Máxima de conclusão'])
 
-console.log(startDate)
-console.log(EndDate)
+let list = [];
+
+for (const job of orderedJobs) {
+    const date = new Date(job['Data Máxima de conclusão']).getTime();
+    const jobDuration = parseInt(job['Tempo estimado']);
+  
+    const existingListIndex = list.findIndex(item => {
+        const existingDate = new Date(item[0]['Data Máxima de conclusão']).getTime();
+        const totalHours = item.reduce((total, currentJob) => total + parseInt(currentJob['Tempo estimado']), 0);
+        
+        return existingDate === date && totalHours + jobDuration <= 8;
+    });
+    
+    if (existingListIndex !== -1) {
+      list[existingListIndex].push(job);
+    } else {
+      list.push([job]);
+    }
+}
+
+console.log(list);
+
+  
+  
